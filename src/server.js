4d2f31a934eaed8c83b6e48e4a131eaf058c9630
Qianchang_official <qianchanglys@gmail.com>
2025-12-30 17:51:56 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-// entry point
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Image AIGC Gateway is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
